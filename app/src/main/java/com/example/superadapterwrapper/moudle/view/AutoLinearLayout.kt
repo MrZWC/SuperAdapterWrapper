4622c22b653527842d68ebf0a739714e3ad77573
左唯辰 <zuoweichen@yineng.com.cn>
2023-05-16 17:12:34 +0800

    update

--- app/src/main/java/com/example/superadapterwrapper/moudle/view/AutoLinearLayout.kt
+++ app/src/main/java/com/example/superadapterwrapper/moudle/view/AutoLinearLayout.kt
@@ -10,8 +10,6 @@ import android.view.ViewGroup
  * Description:描述
  */
 class AutoLinearLayout : ViewGroup {
-    private var totalWSize = 0
-
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
@@ -33,57 +31,58 @@ class AutoLinearLayout : ViewGroup {
         // 计算出所有的childView的宽和高
         measureChildren(widthMeasureSpec, heightMeasureSpec)
         var cWidth = 0
-        var cHeight = 0
         val count = childCount
         if (widthMode == MeasureSpec.EXACTLY) {
             //如果布局容器的宽度模式是确定的（具体的size或者match_parent），直接使用父窗体建议的宽度
             layoutWidth = sizeWidth
-            //动态计算高度
-            if (heightMode == MeasureSpec.EXACTLY) {
-                layoutHeight = sizeHeight
-            } else {
-                var tempWidth = 0//临时宽度
-                var maxHeight = 0
-                var tagLayoutHeight = 0
-                for (i in 0 until count) {
-                    val child = getChildAt(i)
-                    tempWidth += child.measuredWidth//单行宽度
-                    cHeight = child.measuredHeight
-                    if (tempWidth > layoutWidth) {
-                        tagLayoutHeight = layoutHeight
-                        //换行
-                        //重置tempWidth
-                        tempWidth = child.measuredWidth
-                        layoutHeight = tagLayoutHeight + cHeight
-                        maxHeight = cHeight
-                    } else {
-                        maxHeight = if (cHeight > maxHeight) cHeight else maxHeight
-                        layoutHeight = tagLayoutHeight + maxHeight
-                    }
-                }
-            }
-
         } else {
             //如果是未指定或者wrap_content，我们都按照包裹内容做，宽度方向上只需要拿到所有子控件中宽度做大的作为布局宽度
             for (i in 0 until count) {
                 val child = getChildAt(i)
-                cWidth = child.getMeasuredWidth()
-                //获取子控件最大宽度
-                layoutWidth = if (cWidth > layoutWidth) cWidth else layoutWidth
+                cWidth = child.measuredWidth
+                layoutWidth += cWidth
+                if (layoutWidth >= sizeWidth) {
+                    layoutWidth = sizeWidth
+                    break
+                }
             }
         }
-
-        /*  if (heightMode == MeasureSpec.EXACTLY) {
-              layoutHeight = sizeHeight
-          } else {
-              for (i in 0 until count) {
-                  val child = getChildAt(i)
-                  cHeight = child.measuredHeight
-                  layoutHeight = if (cHeight > layoutHeight) cHeight else layoutHeight
-
-              }
-          }*/
+        //动态计算高度
+        layoutHeight = if (heightMode == MeasureSpec.EXACTLY) {
+            sizeHeight
+        } else {
+            measuredHeight(layoutWidth)
+        }
         setMeasuredDimension(layoutWidth, layoutHeight)
+    }
+
+    private fun measuredHeight(
+        layoutWidth: Int,
+    ): Int {
+        var count = childCount
+        var cHeight = 0
+        var layoutHeight = 0
+        var tempWidth = 0//临时宽度
+        var maxHeight = 0
+        var tagLayoutHeight = 0
+        for (i in 0 until count) {
+            val child = getChildAt(i)
+            tempWidth += child.measuredWidth//单行宽度
+            cHeight = child.measuredHeight
+            if (tempWidth > layoutWidth) {
+                tagLayoutHeight = layoutHeight
+                //换行
+                //重置tempWidth
+                tempWidth = child.measuredWidth
+                layoutHeight = tagLayoutHeight + cHeight
+                //重置maxHeight
+                maxHeight = cHeight
+            } else {
+                maxHeight = if (cHeight > maxHeight) cHeight else maxHeight
+                layoutHeight = tagLayoutHeight + maxHeight
+            }
+        }
+        return layoutHeight
     }
 
     /**
